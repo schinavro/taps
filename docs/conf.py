3186c0dd1cde602c266ce2603b4db2c7c0db29ad
me <schinavro@gmail.com>
2021-01-05 21:37:18 +0900

    Para 2

--- docs/conf.py
+++ docs/conf.py
@@ -57,3 +57,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+autodoc_mock_imports = ["kimpy"]
