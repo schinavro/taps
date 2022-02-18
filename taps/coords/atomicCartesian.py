74c8961e8dc3376cdc9cd1bdfa20ac87df33790d
me <schinavro@gmail.com>
2022-02-18 15:15:03 +0900

    ha

--- taps/coords/atomicCartesian.py
+++ taps/coords/atomicCartesian.py
@@ -1,5 +1,5 @@
 from taps.coords import Cartesian
-from taps.utils.arraywrapper import arraylike
+
 
 # @arraylike
 class AtomicCartesian(Cartesian):
@@ -7,7 +7,6 @@ class AtomicCartesian(Cartesian):
     def __int__(self, mass=None, **kwargs):
         self.mass = mass
         super().__init__(**kwargs)
-
 
     def similar(self):
         return self.__class__(coords=None, unit=self.unit, epoch=self.epoch,
